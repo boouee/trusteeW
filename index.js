4d0efd5124ea84b31a30530b9b5b883e3b8afca1
boouee <104202645+boouee@users.noreply.github.com>
2023-12-16 21:12:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @format
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
-import './polyfills'
+import 'polyfills'
 import 'react-native-gesture-handler'
 
 import { AppRegistry } from 'react-native'
