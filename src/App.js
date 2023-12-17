59db1db1cb6826cfaca62629b35e1373589d30ca
boouee <104202645+boouee@users.noreply.github.com>
2023-12-17 19:30:45 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,15 @@
-/**
- * @version 0.43
- * Init App
- */
-import React from 'react'
+import React from 'react';
 import {SafeAreaView, StatusBar, Text, View} from 'react-native';
-import { Provider } from 'react-redux'
-import { AppearanceProvider } from 'react-native-appearance'
-import { enableScreens } from 'react-native-screens'
-
-import store from '@app/store'
-
-import Router from '@app/router'
-import { ThemeProvider } from '@app/theme/ThemeProvider'
-
-import Application from '@app/appstores/Actions/App/App'
-
-enableScreens()
-
-export default class App extends React.Component {
-
-    componentDidMount() {
-        Application.init({ source: 'App.mount', onMount : true })
-    }
-
-    componentWillUnmount() {
-        Application.willUnmount()
-    }
-
-    render() {
-        return (
-    <SafeAreaView>
+ 
+const App = () => {
+ return (
+   <SafeAreaView>
      <StatusBar />
-       <View>
-            <Text style={{fontSize: 24}}>Hello World</Text>
-            <AppearanceProvider>
-                <ThemeProvider>
-                    <Provider store={store}>
-                        <Router />
-                    </Provider>
-                </ThemeProvider>
-            </AppearanceProvider>
-          </View>
+     <View>
+       <Text style={{fontSize: 24}}>Hello World</Text>
+     </View>
    </SafeAreaView>
-        )
-    }
-
-}
+ );
+};
+ 
+export default App;
