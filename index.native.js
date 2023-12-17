a6880eb48796ecbd5cc157f34dff41d2cb6f9ffb
boouee <104202645+boouee@users.noreply.github.com>
2023-12-17 19:24:41 +0300

    Update index.native.js

--- index.native.js
+++ index.native.js
@@ -1,5 +1,31 @@
-import {AppRegistry} from 'react-native';
-import App from './src/App';
-import {name as appName} from './src/app.json';
+/**
+ * @format
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+import 'polyfills'
+import 'react-native-gesture-handler'
 
-AppRegistry.registerComponent(appName, () => App);
+import { AppRegistry } from 'react-native'
+import './shim.js'
+import './global'
+import App from './src/App'
+import { name as appName } from './src/app.json'
+
+import PushNotification from 'react-native-push-notification'
+
+import MarketingEvent from '@app/services/Marketing/MarketingEvent'
+import AppNotificationPopup from '@app/services/AppNotification/AppNotificationPopup'
+
+MarketingEvent.initMarketing(false, true)
+
+PushNotification.configure({
+    // (required) Called when a remote is received or opened, or local notification is opened
+    onNotification: function(notification) {
+        AppNotificationPopup.onOpened(notification)
+    }
+})
+
+
+// @deprecated BackgroundFetch.registerHeadlessTask(BackgroundDaemon.taskToRegister)
+AppRegistry.registerComponent(appName, () => App)
+
